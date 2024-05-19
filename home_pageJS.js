3e7c74be5b750e41a0eca70ff2d752f79542751a
GabrielDuran1 <gabrieleduran360@gmail.com>
2024-05-19 08:17:54 -0400

    Re-using code
    Didn't realize this code was good all along

--- home_pageJS.js
+++ home_pageJS.js
@@ -1,8 +1,32 @@
-document.getElementById("nameForm").onsubmit = function(e) {
+document.getElementById('nameForm').onsubmit = function(e) {
+    console.log("Form submission triggered");
     e.preventDefault();
-    var firstName = document.getElementById("firstName").value;
-    var lastName = document.getElementById("lastName").value;
+
+    var firstName = document.getElementById('firstName').value;
+    var lastName = document.getElementById('lastName').value;
+
     console.log("First Name:", firstName);
     console.log("Last Name:", lastName);
+
     modal.style.display = "none"; 
-}
+
+    var formData = {
+        firstName: firstName,
+        lastName: lastName
+    };
+
+    fetch('/customer', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+};
